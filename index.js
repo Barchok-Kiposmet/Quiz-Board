0acacb8bf2a7124a534e9099d2cbc3d83369b6f1
Barchok-Kiposmet <barchokpatrick@gmail.com>
2022-12-16 16:51:58 +0300

    port updated

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.port || 3000;
+
 app.get('/', function(req, res){
     res.send('<p>This is some HTML page</p>');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('My server is running');
 });
