188bd075ce6154e56c40c0be68f5664acaa6b0ab
Barchok-Kiposmet <barchokpatrick@gmail.com>
2023-01-30 12:34:50 +0300

    added view engines

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const port = process.env.port || 3000;
 
 app.get('/', function(req, res){
-    res.send('<p>This is some HTML page</p>');
+    res.send('index');
 });
 
 app.listen(port, () => {
